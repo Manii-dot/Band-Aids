247fb8e0f4fe7f0282be7671fc46b0ca068c4a99
Austin Lambert <atlambert11@gmail.com>
2020-11-04 11:52:54 -0600

    Finished addEvent.js

--- public/js/addEvent.js
+++ public/js/addEvent.js
@@ -4,15 +4,18 @@ $(document).ready(function() {
     var inputLocation = $("#inputLocation");
     var imageURL = $("#imageURL");
     var inputDescription = $("#caption");
+    var inputDate = $("#date");
     var inputGenre = $("#category");
 
     eventForm.on("submit", function(event) {
         event.preventDefault();
+        // Grab values from form
         var eventData = {
             band: inputBand.val().trim(),
-            location: inputLocation.val().trim(),
+            place: inputLocation.val().trim(),
             image: imageURL.val().trim(),
             description: inputDescription.val().trim(),
+            date: inputDate.val().trim(),
             genre: inputGenre.val().trim()
         };
 
@@ -20,13 +23,22 @@ $(document).ready(function() {
     });
 
     function createEvent(data) {
-        let { band, location, image, description, genre } = data;
-
+        // Deconstructed values
+        let { band, place, image, description, date, genre } = data;
+        //Post data through api route
         $.post("/api/addevent", {
             band: band,
-            location: location,
-
-        })
-
+            place: place,
+            image: image,
+            description: description,
+            date: date,
+            genre: genre
+        }).then(function(data) {
+            // Redirect to members.html
+            window.location.replace("members");
+        }).catch(function(err) {
+            // Thow error
+            if (err) throw err;
+        });
     }
 });
