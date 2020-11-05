d5369cd47e9d975dbb698c0d2e8ea955c85dfb53
Austin Lambert <atlambert11@gmail.com>
2020-11-05 11:27:10 -0600

    bleh

--- models/user.js
+++ models/user.js
@@ -19,11 +19,11 @@ module.exports = function(sequelize, DataTypes) {
         }
     });
 
-    User.associate = models => {
-        User.hasMany(models.Event, {
-            onDelete: "cascade"
-        });
-    }
+    // User.associate = models => {
+    //     User.hasMany(models.Event, {
+    //         onDelete: "cascade"
+    //     });
+    // }
 
 
     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
