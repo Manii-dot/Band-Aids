7e5b3195fe9372877ab51e25eb39860adc590800
Zinc <greenberg.zachary@utexas.edu>
2020-11-05 14:15:04 -0600

    code for nodemailer

--- public/js/mailer.js
+++ public/js/mailer.js
@@ -1,29 +1,38 @@
 let nodemailer = require('nodemailer');
-let cron = require('node-cron');
+var db = require("./models");
 
-//e-mail message options
-let mailOptions = {
-    from: '<FROM_EMAIL_ADDRESS>',
-    to: '<TO_EMAIL_ADDRESS>',
-    subject: 'Email from Node-App: A Test Message!',
-    text: 'Some content to send'
-};
+// cron.schedule('* * * * *', () => {
+//     transporter.sendMail(mailOptions, function(error, info){
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log('Email sent: ' + info.response);
+//         }
+//     });
+// });
 
-//e-mail transport configuration
+// create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'gmail',
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
-      user: '<FROM_EMAIL_ADDRESS>',
-      pass: '<FROM_EMAIL_PASSWORD>'
-    }
-});
+      user: "band.aid.nodemailer@gmail.com",
+      pass: "Test#Test2020",
+    },
+  });
 
-cron.schedule('* * * * *', () => {
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
+  // send mail with defined transport object
+  let mailOptions = {
+    from: '"Band-Aid Development Team <band.aid.nodemailer@gmail.com>',
+    to: '${db.User.email}',
+    subject: 'Welcome to Band-Aid!',
+    html: '<h1>Welcome to Band-Aid!</h1><p>We are excited to have a new member share, explore, and discover music events happening around the world!</p>'
+};
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 });
